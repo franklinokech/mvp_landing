55be05f3138bed5379e352f0fc2723cee2f925d3
Franklin okech <franklinokech@gmail.com>
2020-02-23 11:47:49 +0300

    handled model forms and custom forms

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import SignUp
 
 
+class ContactForm(forms.Form):
+    full_name = forms.CharField()
+    email = forms.EmailField()
+    message = forms.CharField()
+
+
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
@@ -12,8 +18,8 @@ class SignUpForm(forms.ModelForm):
         email_base, provider = email.split('@')
         domain, extension = provider.split('.')
 
-        if not domain == 'USC':
-            raise forms.ValidationError('Please make sure you use the USC email')
+        # if not domain == 'USC':
+        #     raise forms.ValidationError('Please make sure you use the USC email')
         if not extension == 'edu':
             raise forms.ValidationError('Please make sure you use the .edu email')
 
@@ -22,7 +28,7 @@ class SignUpForm(forms.ModelForm):
     def clean_full_name(self):
         full_name = self.cleaned_data.get('full_name')
 
-        if not full_name == 'franklin':
-            raise forms.ValidationError('Name must be franklin')
+        # if not full_name == 'franklin':
+        #     raise forms.ValidationError('Name must be franklin')
 
         return full_name
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
+from .forms import SignUpForm, ContactForm
 
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "newsletter/home.html", {})
+    title = 'Welcome'
+    form = SignUpForm(request.POST or None)
+
+    context = {
+        'title': title,
+        'form': form,
+    }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        my_full_name = form.cleaned_data.get('full_name')
+        my_email = form.cleaned_data.get('email')
+
+        print(my_full_name)
+        print(my_email)
+
+        instance.save()
+
+        context = {
+            'title': 'Thank You'
+        }
+
+    return render(request, "newsletter/home.html", context)
+
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+
+    if form.is_valid():
+        for key, value in form.cleaned_data.items():
+            print(key, value)
+        # email = form.cleaned_data.get('email')
+        # full_name = form.cleaned_data.get('full_name')
+        # message = form.cleaned_data.get('message')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'newsletter/contact.html', context)
